9369f7e5561172d56bfcb8c567da3c2a4f2a9ef5
Tom Palmer <tjpalmer@tjpalmer.com>
2013-05-07 13:08:08 -0500

    Both on the same page.

---
+++ blockly-mario.js
@@ -0,0 +1,15 @@
+// TODO Replace with Typescript code!
+
+(function() {
+
+window.onload = function() {
+  // TODO Support telling mario where to load things!
+  new Enjine.Application().Initialize(new Mario.LoadingState(), 320, 240);
+  Blockly.inject($('blockly'), {path: "blockly/", toolbox: $('toolbox')});
+};
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+})();
