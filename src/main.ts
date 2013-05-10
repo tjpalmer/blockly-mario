a4d9b2d7676c040b24337f0e11a43798e9037d57
Tom Palmer <tjpalmer@tjpalmer.com>
2013-05-10 00:02:55 -0500

    AI control.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@
 
 module blockly_mario {
 
+var aiFunction: () => void;
+
 var application: any;
 
 window.onload = function() {
@@ -20,31 +22,60 @@ window.onload = function() {
 
   // Event handlers.
   // Reset pause because we otherwise just get a blank canvas.
-  (<HTMLInputElement>$('pause')).checked = false;
+  $input('pause').checked = false;
   $('pause').onclick = handlePause;
   $('update').onclick = updateCode;
+
+  // Disable AI control until update.
+  $input('ai').disabled = true;
 };
 
 class AiUpdate {
   constructor(private base: any) {}
   Update(delta: number): void {
     // TODO Mario AI calls!
+    var aiActive = $input('ai').checked;
+    var oldPressed: any;
+    if (aiActive) {
+      // Copy keys pressed for later restore.
+      oldPressed = Enjine.KeyboardInput.Pressed;
+      Enjine.KeyboardInput.Pressed = {};
+      Enjine.KeyboardInput.Element = null;
+      if (aiFunction) {
+        aiFunction();
+      }
+    }
+    // Run the base update either way.
     this.base.Update(delta);
+    if (aiActive) {
+      // Restore old keyboard layout.
+      Enjine.KeyboardInput.Pressed = oldPressed;
+      Enjine.KeyboardInput.Element = $('canvas');
+    }
   }
 }
 
-function $(id): HTMLElement {
-  return document.getElementById(id);
+function $(id) => <HTMLElement>document.getElementById(id);
+
+function $input(id) => <HTMLInputElement>$(id);
+
+function copySimpleShallow(object) {
+  var copy = {};
+  for (var key in object) {
+    copy[key] = object[key];
+  }
+  return copy;
 }
 
 function handleKeyDown(base) {
   // Using function here because => ties this, and I want standard one.
   return function(event: KeyboardEvent) {
+    // TODO Add space code (32) to Enjine.Keys?
     if (this.IsActive() && event.keyCode == 32) {
       // Toggle on space.
       // Handle this at the event itself so we don't need to track from time to
       // time.
-      var pause = <HTMLInputElement>$('pause');
+      var pause = $input('pause');
       pause.checked = !pause.checked;
       handlePause();
     } else {
@@ -55,7 +86,7 @@ function handleKeyDown(base) {
 }
 
 function handlePause() {
-  if ((<HTMLInputElement>$('pause')).checked) {
+  if ($input('pause').checked) {
     application.timer.Stop();
   } else {
     application.timer.Start();
@@ -64,7 +95,21 @@ function handlePause() {
 
 function updateCode() {
   var code = Blockly.Generator.workspaceToCode('JavaScript');
-  console.log(code);
+  // Wrap in a function we can call at each update.
+  // TODO Do I want to capture or use time delta?
+  code = ["(function() {", code, "})"].join("\n");
+  //console.log(code);
+  try {
+    aiFunction = eval(code);
+    $input('ai').disabled = false;
+  } catch (e) {
+    alert("Error building code.");
+    aiFunction = null;
+    $input('ai').checked = false;
+    $input('ai').disabled = true;
+    // Disable AI control.
+    throw e;
+  }
 }
 
 }
